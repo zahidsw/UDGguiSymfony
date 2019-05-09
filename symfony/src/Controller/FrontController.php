cf0ab0916a7f017159b66e5a2560129cb317641a
Stefano Maglione <smaglione@mandint.org>
2019-05-09 18:01:46 +0200

    automatic deployment

--- symfony/src/Controller/FrontController.php
+++ symfony/src/Controller/FrontController.php
@@ -6,18 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use App\Entity\Gui\Product;
+use App\Form\VNOType;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Psr\Log\LoggerInterface;
+
 
 
 class FrontController extends AbstractController
 {
+    private $logger;
 
-	
 
-    public function __construct()
+
+    public function __construct(LoggerInterface $logger)
     {
-		
+        $this->logger = $logger;
     }
-    
 
     public function list()
     {
@@ -70,5 +77,78 @@ class FrontController extends AbstractController
     {
         return $this->render('frontpage/service.html.twig');
     }
+
+
+
+
+    public function createVno(Request $request)
+    {
+        $form = $this->createForm(VNOType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            
+            $task = $form->getData();
+
+            $pop = array (
+                'name' => $task['name'],
+                'authUrl' => $task['AuthorisationURL'],
+                'tenant' => $task['Tenant'],
+                'username' => $task['Username'],
+                'password' => '65S8xLcC57',
+                'keyPair' => $task['KeyPair'],
+                'securityGroups' => $task['SecurityGroups'],
+                'type' => $task['Type'],
+                'location' => 
+                array (
+                  'name' => 'Aarhus',
+                  'latitude' => '56.162939',
+                  'longitude' => '10.203921',
+                ),
+            );
+
+            $filesystem = new Filesystem();
+            $this->logger->info('Creating file: /tmp/pop.json');
+            
+            $filesystem->dumpFile('/tmp/pop.json', json_encode($pop));
+            $this->logger->info('Executing: slice-manager --pop-descriptor /tmp/pop.json');
+            $process = new Process(['slice-manager', '--pop-descriptor','/tmp/pop.json']);
+            $process->run();
+
+            // executes after the command finishes
+
+            try {
+                if (!$process->isSuccessful()) 
+                {
+                    throw new ProcessFailedException($process);
+                }
+            }
+            catch (\Exception $e) {
+                $this->logger->error($e->getMessage());
+            }
+           
+
+            $this->logger->info($process->getOutput());
+            
+            return $this->redirectToRoute('vnocreate');
+        }
+
+
+
+
+        $data['form'] = $form->createView();
+    
+    	return $this->render('frontpage/vnoAdd.html.twig', $data);
+
+
+    }
+
+
+
+
+
+
 
 }
---
+++ symfony/src/Form/VNOType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+
+class VNOType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('name',			   TextType::class)
+			->add('AuthorisationURL',  TextType::class)
+			->add('Tenant',	           TextType::class)
+			->add('Username',          TextType::class)
+            ->add('Type', ChoiceType::class, [
+                'choices'  => [
+                    'openstack' => 'openstack',
+                ],
+            ])
+            ->add('KeyPair',	       TextType::class)
+            ->add('ProjectId',	       TextType::class)
+            ->add('SecurityGroups', ChoiceType::class, [// drop down multiple selection
+                'choices'  => [
+                    'default' => 'default',
+                    'ICMP campus' => 'ICMP campus',
+                    'ssh-fc00' => 'ssh-fc00',
+                    'udg_network'=> 'udg_network',
+                    'ssh-campus' => 'ssh-campus',
+                    'icmp6' => 'icmp6',
+                    'icmp10.0.0.0' => 'icmp10.0.0.0',
+                    'openbaton' => 'openbaton',
+                ],
+                'multiple' => true,
+                'expanded' => false,
+            ])
+			;
+	}
+	
+	
+}
