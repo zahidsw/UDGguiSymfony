4b2fd26fb51f7acca1ccfb123d0e816f5b62116b
Stefano Maglione <smaglione@mandint.org>
2019-08-13 12:39:18 +0200

    update

--- symfony/src/Controller/PurchaseController.php
+++ symfony/src/Controller/PurchaseController.php
@@ -193,7 +193,7 @@ class PurchaseController extends AbstractController
                     'last_name' => 'Client',
                     'email' => 'felpone84@gmail.com',
                     'street' => 'Buchanan St. 21',
-                    'city' => 'versoix',
+                    'city' => 'belgrade',
                     'state' => 'Country',
                     'zip_code' => '94147',
                     'country' => 'Country',
